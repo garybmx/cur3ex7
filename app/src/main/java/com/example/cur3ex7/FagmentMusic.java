e48e50733d97ceab2523eb19ca07b720852c2434
Igor U <garymtb@yandex.ru>
2019-10-10 00:10:54 +0300

    Tabs + ViewPager and BottonNavigationView

---
+++ app/src/main/java/com/example/cur3ex7/FagmentMusic.java
@@ -0,0 +1,27 @@
+package com.example.cur3ex7;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FagmentMusic  extends Fragment {
+
+    public static FagmentMusic newInstance(Bundle bundle) {
+        FagmentMusic currentFragment = new FagmentMusic();
+        Bundle args = new Bundle();
+        args.putBundle("gettedArgs", bundle);
+        currentFragment.setArguments(args);
+        return currentFragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fr_music, container, false);
+        return rootView;
+    }
+
+}
